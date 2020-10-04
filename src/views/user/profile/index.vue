0fd173141fe1a3efc49a2ae3cc3c951c6ea0e972
injahow <i@injahow.com>
2020-10-04 22:41:10 +0800

    feat: add user profile options

--- src/views/user/profile/index.vue
+++ src/views/user/profile/index.vue
@@ -8,29 +8,42 @@
         :label="tabLabel[i]"
         :name="i"
       >
-        <el-form :ref="formData[i]" :model="formData[i]" label-width="80px">
-          <el-form-item v-for="j in optionForm[i]" :key="'form_'+j" :label="optionLabel[j]" :prop="j">
-            <el-select
-              v-model="formData[i][j]"
-              multiple
-              filterable
-              allow-create
-              default-first-option
-              style="width: 100%;"
-              placeholder="请添加选项"
-            >
-              <el-option
-                v-for="k in formData[i][j]"
-                :key="'option_'+j+'_'+k"
-                :label="k"
-                :value="k"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm()">保存</el-button>
-          </el-form-item>
-        </el-form>
+        <div v-if="i === 'user'">
+          <el-form :ref="formData[i]" :model="formData[i]" label-width="80px">
+            <el-form-item v-for="j in optionForm[i]" :key="'form_'+j" :label="optionLabel[j]" :prop="j">
+              <el-input v-model="formData[i][j]" placeholder="请输入内容" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm()">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div v-if="i !== 'user'">
+          <el-form :ref="formData[i]" :model="formData[i]" label-width="80px">
+            <el-form-item v-for="j in optionForm[i]" :key="'form_'+j" :label="optionLabel[j]" :prop="j">
+              <el-select
+                v-model="formData[i][j]"
+                multiple
+                filterable
+                allow-create
+                default-first-option
+                style="width: 100%;"
+                placeholder="请添加选项"
+              >
+                <el-option
+                  v-for="k in formData[i][j]"
+                  :key="'option_'+j+'_'+k"
+                  :label="k"
+                  :value="k"
+                />
+              </el-select>
+
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm()">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -59,7 +72,12 @@ export default {
       activeName: 'user', // i-3
       optionForm: { // j-1
         'user': [
-          'no_name'
+          'avatar',
+          'introduction',
+          'github_name',
+          'weibo_name',
+          'twitter_name',
+          'facebook_name'
         ],
         'anime': [
           'type_name',
@@ -87,7 +105,13 @@ export default {
         'actor': 'Actor选项',
         'staff': 'Staff选项',
         'region': '地区选项',
-        'no_name': '无名'
+        'no_name': '无名',
+        'avatar': '头像',
+        'introduction': '介绍',
+        'github_name': 'GitHub',
+        'weibo_name': '微博',
+        'twitter_name': 'Twitter',
+        'facebook_name': 'Facebook'
       },
       oldFormData: {},
       needUpdateOld: {
@@ -122,6 +146,8 @@ export default {
         }
       }
     }
+  }, mounted() {
+    this.handleClick({ name: 'user' })
   },
   methods: {
     submitForm() {
@@ -136,8 +162,9 @@ export default {
       this.$refs[formName].resetFields()
     },
     handleClick(event) {
-      // 注意name规范化
+      // 注意name规范化，user特殊
       const name = event.name
+      if (name === 'other') return // 占位
       if (this.needUpdateOld[name]) {
         getOptions(name).then((res) => {
           if (res.data) {
